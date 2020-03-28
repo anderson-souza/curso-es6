782f093958ef69fe4499af673d2567bc524122d9
Anderson <andersonpds14@gmail.com>
2020-03-28 12:27:45 -0300

    Operacoes map, reduce, filter e find

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
-class List {
-    constructor() {
-        this.data = [];
-    }
+const arr = [1, 5, 4, 6, 9];
 
-    add(data) {
-        this.data.push(data);
-        console.log(this.data);
-    }
-}
+const newArray = arr.map(function (item, index) {
+    return item + index;
+});
 
+console.log(newArray);
 
-class TodoList extends List {
+const sum = arr.reduce(function (total, next) {
+    return total + next;
+});
 
-    constructor() {
-        super();
-        this.usuario = 'Anderson';
-    }
+console.log(sum);
 
-    mostraUsuario() {
-        console.log(this.usuario);
-    }
+const filter = arr.filter(function (item) {
+    return item % 2 === 0;
+});
 
-}
+console.log(filter);
 
-var MinhaLista = new TodoList();
+const find = arr.find(function (item) {
 
-document.getElementById('novoTodo').onclick = function () {
-    MinhaLista.add('Novo Todo');
-}
+    return item === 4;
 
-MinhaLista.mostraUsuario();
+});
+
+console.log(find);
