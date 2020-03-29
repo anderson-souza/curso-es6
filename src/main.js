08c7e16d57fb0a58abf8dbde0ff26badbdf8f45b
Anderson <andersonpds14@gmail.com>
2020-03-29 18:06:10 -0300

    Adicionado plugins para Async e Await Criado exemplos com Async

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-alert('Salve');
+const minhaPromise = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('ok') }, 2000);
+});
+
+//Forma padrão utilizando Promisse
+minhaPromise()
+    .then(response => {
+        console.log("Promisse: " + response);
+    })
+    .catch(err => {
+
+    });
+
+// async function executaPromisse() {
+//     const response = await minhaPromise();
+//     // A próxima linha só será executada assim que a linha superior for finalizada
+//     console.log("Async: " + response);
+// }
+
+const executaPromisse = async () => console.log(await minhaPromise())
+
+executaPromisse();
