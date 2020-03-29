fd28efd1269a8d4d1a925fa72ea246096003b3e8
Anderson <andersonpds14@gmail.com>
2020-03-29 17:42:58 -0300

    Criado arquivo para armazenar funções; Criado arquivo com função de soma; Exemplos utilizando várias formas de importação.

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-import { soma } from './funcoes'
+// import { divisao, soma as somatoria, sub } from './funcoes' //Utiliza-se as chaves quando se quer importar uma função específica
+import * as funcoes from './funcoes' //importa todas as funções do arquivo
+// import somaFunction from './soma' //Não precisa utilizar chaves quando se importa uma função default e é possível alterar seu nome na importação
 
-console.log(soma(1, 2));
+
+// console.log(somaFunction(1, 2));
+// console.log(somatoria(1, 2));
+// console.log(sub(1, 2));
+// console.log(divisao(1, 2));
+
+
+console.log(funcoes.divisao(1, 2));
+console.log(funcoes.soma(1, 2));
+console.log(funcoes.sub(1, 2));
