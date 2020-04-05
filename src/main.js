6d9bb460606a785aad3de4db02641f71d234e2f9
Anderson <andersonpds14@gmail.com>
2020-04-05 11:27:10 -0300

    Adicionado tratamento de erros e loading na busca da API

--- src/main.js
+++ src/main.js
@@ -17,6 +17,18 @@ class App {
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
+    setLoading(loading = true) {
+        if (loading) {
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando'));
+            loadingEl.setAttribute('id', 'loading');
+
+            this.formEl.appendChild(loadingEl);
+        } else {
+            document.getElementById('loading').remove();
+        }
+    }
+
     async addRepository(event) {
         event.preventDefault();
 
@@ -26,20 +38,27 @@ class App {
             return;
         }
 
-        const response = await api.get(`/repos/${repoInput}`);
+        this.setLoading();
 
-        const { name, description, html_url, owner: { avatar_url } } = response.data;
+        try {
+            const response = await api.get(`/repos/${repoInput}`);
 
-        this.repositories.push({
-            name: name,
-            description: description,
-            avatar_url: avatar_url,
-            html_url: html_url,
-        });
+            const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+            this.repositories.push({
+                name: name,
+                description: description,
+                avatar_url: avatar_url,
+                html_url: html_url,
+            });
+
+            this.render();
+        } catch (error) {
+            alert('Ocorreu um erro na requisição: ' + error)
+        }
 
         this.inputEl.value = '';
-
-        this.render();
+        this.setLoading(false);
     }
 
     render() {
